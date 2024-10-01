915a216b367fa50168ee83bb4d1b3d9713a9ae46
Rafal <rafalkunickidev@gmail.com>
2024-10-01 21:04:55 +0100

    work with navbar

---
+++ src/Pages/Home/Navbar.jsx
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom";
+import classes from "./Navbar.module.css";
+import { DiReact } from "react-icons/di";
+const Navbar = () => {
+  return (
+    <div className={classes.navbarSection}>
+      <div className={classes.navbarLogo}>
+        <span className={classes.logo}>
+          <DiReact />
+        </span>
+      </div>
+      <div className={classes.navbarListWrapper}>
+        <ul className={classes.navbarLinks}>
+          <li>
+            <Link>Home</Link>
+          </li>
+          <li>
+            <Link>Portfolio</Link>
+          </li>
+          <li>
+            <Link>AboutMe</Link>
+          </li>
+          <li>
+            <Link>Contact Me</Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+export default Navbar;
