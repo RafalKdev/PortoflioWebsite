ff72543fdc875cf554fe4f9031244d872f182fb4
Rafal <rafalkunickidev@gmail.com>
2024-10-22 20:58:13 +0100

    skillsSectionDone

--- src/Pages/Home/MySkills.jsx
+++ src/Pages/Home/MySkills.jsx
@@ -4,17 +4,17 @@ import classes from "./MySkills.module.css";
 const MySkills = () => {
   return (
     <section className={classes.skillsSection}>
-      <div>
-        <p>My Skills</p>
-        <h2>My Expertise</h2>
+      <div className={classes.skillsHeaderContainer}>
+        <div className={classes.skillsHeader}>
+          <h2 className={classes.skillsHeaderTitle}>My Skills</h2>
+        </div>
       </div>
-      <div>
+
+      <div className={classes.skillsContainer}>
         {data?.skills?.map((item, index) => (
-          <div key={index}>
-            <div>
-              <img className={classes.skillsImg} src={item.src} alt="photo" />
-            </div>
-            <div>
+          <div key={index} className={classes.skillsSectionCard}>
+            <img className={classes.skillsImg} src={item.src} alt="photo" />
+            <div className={classes.cardContent}>
               <h3>{item.title}</h3>
               <p>{item.description}</p>
             </div>
